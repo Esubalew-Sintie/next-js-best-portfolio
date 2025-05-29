8378a661b184cb0d77404c5e819b334edaea5689
esubalewsintie <esubalewsintie1302@gmail.com>
2025-05-29 09:57:49 +0300

    my best portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
-import { ThemeProvider } from "@/provider/ThemeProvider";
+// import { ThemeProvider } from "@/provider/ThemeProvider";
 import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
